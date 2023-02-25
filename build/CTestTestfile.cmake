122cfb424fdadea9abdf0f651446e21c1cd4b387
Nils Drueke <nils@drueke.email>
2023-02-25 17:56:36 +0100

    cleared build path

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/c/dev/CppND-ProjectCapstone_BinaryBandit81
-# Build directory: /mnt/c/c/dev/CppND-ProjectCapstone_BinaryBandit81/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
